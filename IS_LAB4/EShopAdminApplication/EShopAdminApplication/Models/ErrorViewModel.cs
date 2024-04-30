67f1509c53880955e0aaf1297d1aced588c211c6
eroolls4 <eroollsaqipii@gmail.com>
2024-04-30 14:03:39 +0200

    Revert "lab4 added"
    This reverts commit fd34dad15b941cb45e7231f75db8fa4cd80243c7.

--- IS_LAB4/EShopAdminApplication/EShopAdminApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EShopAdminApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
