8f98336e73df04a70ed2f04c7db10f1e5f2faaf5
eroolls4 <eroollsaqipii@gmail.com>
2024-05-14 23:24:24 +0200

    import working
    zzz

---
+++ Lab5/integrated_systems/EShop.Domain/DTO/ConcertRegistrationDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.DTO
+{
+    public class ConcertRegistrationDTO
+    {
+       
+        public string ConcertName { get; set; }
+      
+        public string ConcertDescription { get; set; }
+     
+        public string ConcertImage { get; set; }
+    
+        public double  Rating  { get; set; }
+
+       
+
+    }
+}
